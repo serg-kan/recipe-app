00202faa51ea1de12ce52b0e92e51756841d264c
serg-kan <serg.kan096@gmail.com>
2023-11-01 22:50:28 +0300

    feat: basic layout

---
+++ frontend/components/recipe-card.tsx
@@ -0,0 +1,35 @@
+import * as React from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+export default function RecipeCard() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Курица с рисом</CardTitle>
+        <CardDescription>Описание рецепта</CardDescription>
+      </CardHeader>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+  )
+}
